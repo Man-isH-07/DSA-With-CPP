f1efbc523553a8ef1414ae050e4c5580a51feff3
Man-isH-07 <manishdhaye07@gmail.com>
2024-07-25 21:37:58 +0530

    25 July 2024

---
+++ Code360 - CodingNinjas/Sort 0 1 2.cpp
@@ -0,0 +1,32 @@
+/* Sort 0 1 2
+
+Problem statement
+You have been given an integer array/list(ARR) of size 'N'. It only contains 0s, 1s and 2s. Write a solution to sort this array/list.
+
+Note :
+Try to solve the problem in 'Single Scan'. ' Single Scan' refers to iterating over the array/list just once or to put it in other words, you will be visiting each element in the array/list just once.
+
+*/
+
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+
+}
